8d4b51d2d95f26dc12e104779af7d04a4e6ab4dc
Anastasiia-Shvets <144601223+Anastasiia-Shvets@users.noreply.github.com>
2024-02-25 13:27:15 +0200

    iziToast

--- src/main.js
+++ src/main.js
@@ -17,14 +17,28 @@ async function onFormSubmit(ev) {
     ev.preventDefault();
     query = ev.target.elements.query.value.trim();
     page = 1;
+    if (!query) {
+        showError('Please enter text to search for.');
+        return;
+    }
     showLoader();
-    
-    const data = await searchGallery(query);
-    
-    console.log(data);
+
+    try {
+        const data = await searchGallery(query);
+    if (data.totalHits === 0) {
+        showError(
+            'Sorry, there are no images matching your search query. Please try again!'
+        );
+    }
     maxPage = Math.ceil(data.totalHits / 15);
     refs.listElem.innerHTML = '';
-    renderHits(data.hits);
+    renderHits(data.hits);  
+    } catch (error) {
+        showError(error.message);
+        maxPage = 0;
+        refs.listElem.innerHTML = '';
+    }
+    
     hideLoader();
     checkBtnStatus();
 
@@ -67,4 +81,12 @@ function checkBtnStatus() {
     } else {
         showLodeMore();
     }
+}
+
+function showError(msg) {
+    iziToast.error({
+        title: 'Error',
+        message: msg,
+        position: 'topRight',
+    });
 }
